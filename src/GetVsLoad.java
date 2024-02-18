aee0f228da7a582ff6ac5e30809a340120def86d
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-02-18 16:01:08 +0530

    GetVsLoad get method

---
+++ src/GetVsLoad.java
@@ -0,0 +1,27 @@
+import config.SessionFactoryConfig;
+import entity.Customer;
+import org.hibernate.Session;
+
+public class GetVsLoad {
+
+    public  static  void  main(String[] args){
+
+        Session session = SessionFactoryConfig.getInstance().getSession();
+        Customer customer = new Customer();
+        customer.setName("Saman");
+        customer.setAddress("Kaluthara");
+        session.save(customer);
+        session.close();
+
+        //1.Get
+        Session getSession = SessionFactoryConfig
+                .getInstance().getSession();
+
+        System.out.println("================Get=================");
+        Customer getCustomer = getSession.get(Customer.class,1);
+        System.out.println("Customer Id of Get: " + getCustomer.getId());
+        System.out.println("Customer Name of Get: " + getCustomer.getName());
+        getSession.close();
+
+    }
+}
